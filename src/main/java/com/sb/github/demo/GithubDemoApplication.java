cd8531685465077ee21f02907e9d06fa3485075e
gowtham-2oo5 <gowtham.ala12345@gmail.com>
2024-10-28 15:28:35 +0530

    Initial commit

---
+++ src/main/java/com/sb/github/demo/GithubDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sb.github.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sb/github/demo/GithubDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb.github.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
